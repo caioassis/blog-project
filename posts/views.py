e1f2d2a06106c11a08b9bf98f400ec119f8d04f4
Caio Eduardo Borges Assis <caio@ypytecnologia.com.br>
2019-12-17 17:05:24 -0300

    Post List View

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from posts.models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/home.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(published=True)
+        return queryset
