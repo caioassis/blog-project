ca19851cd261ba13ef39bbdaee6d08f4c56459c1
Caio Eduardo Borges Assis <caio@ypytecnologia.com.br>
2019-12-19 20:59:48 -0300

    Enable media serving in development mode only

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import LoginView, LogoutView, SignupView, AuthorListView, AuthorUpdateView, AuthorDeleteView
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('authors/<int:pk>/edit/', AuthorUpdateView.as_view(), name='author_update'),
     path('authors/<int:pk>/delete/', AuthorDeleteView.as_view(), name='author_delete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
