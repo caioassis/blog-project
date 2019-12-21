5c8cdafdae1300bf13efb08440de8c4a3a614be8
Caio Eduardo Borges Assis <caio@ypytecnologia.com.br>
2019-12-20 23:21:04 -0300

    Added urls for approving post, audit post list, post creation and deletion

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostUpdateView, ReplyCreateView, AuthorPostListView
+from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView, AuthorPostListView,
+                    AuditPostListView, PostDeleteView, ApprovePostView)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
+    path('create/', PostCreateView.as_view(), name='post_create'),
     path('my_posts/', AuthorPostListView.as_view(), name='author_post_list'),
+    path('audit_posts/', AuditPostListView.as_view(), name='audit_posts'),
+    path('<int:pk>/approve/', ApprovePostView.as_view(), name='approve_post'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
-    path('<int:post_id>/replies/create/', ReplyCreateView.as_view(), name='reply_create')
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
